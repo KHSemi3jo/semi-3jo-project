186a927a6c84190f198aef505ec2729ad0e01d99
MJH098 <narutoye@naver.com>
2023-09-21 21:15:12 +0900

    장바구니 담기

--- gogi/src/main/java/com/kh/gogi/rest/BasketRestConrtoller.java
+++ gogi/src/main/java/com/kh/gogi/rest/BasketRestConrtoller.java
@@ -1,14 +1,19 @@
 package com.kh.gogi.rest;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.gogi.dao.BasketDao;
+import com.kh.gogi.dao.ProductDao;
 import com.kh.gogi.dto.BasketDto;
+import com.kh.gogi.dto.ProductDto;
 
 @CrossOrigin
 @RestController
@@ -17,14 +22,22 @@ public class BasketRestConrtoller {
 	@Autowired
 	private BasketDao basketDao;
 	
+	@Autowired
+	private ProductDao productDao;
+	
 	@PostMapping("/add")
-	public String add(@ModelAttribute BasketDto basketDto) {
+	public void add(@RequestParam int productNo, HttpSession session) {
+		ProductDto productDto = productDao.selectOne(productNo);
+		if(productDto == null) return;//없는 상품이면 중지
+		
 		int basketNo = basketDao.sequence();
+		
+		BasketDto basketDto = new BasketDto();
 		basketDto.setBasketNo(basketNo);
-		
+		basketDto.setBasketListNo(productDto.getProductNo());
+		String memberId = (String) session.getAttribute("name");
+		basketDto.setBasketMember(memberId);
 		basketDao.add(basketDto);
-		
-		return "redirect:/basket/basketList";
 	}
 
 }
--- gogi/src/main/webapp/WEB-INF/views/product/list.jsp
+++ gogi/src/main/webapp/WEB-INF/views/product/list.jsp
@@ -2,16 +2,34 @@
     pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <jsp:include page="/WEB-INF/views/template/header.jsp"></jsp:include>
+<script>
+    $(function(){
+        $(".btn-basket").click(function(){
+        	
+            $.ajax({
+                url:"http://localhost:8080/rest/basket/add",
+                method:"post",
+                data:{productNo:productNo},
+                success:function(){
+                    
+                }
+            });
+        });
+    });
+</script>
 
 	<c:forEach var="productDto" items="${list}">
 		<div class="row">
-			<a class="link" href="detail?productNo= ${productDto.productNo}">${productDto.productNo}</a> /
+			<a class="link" href="detail?productNo=${productDto.productNo}">${productDto.productNo}</a> /
 			 ${productDto.productName}/
 			  ${productDto.productPrice}원
 			
 		<a href="add?productNo=${productDto.productNo}">등록</a>
 		<a href="edit?productNo=${productDto.productNo}">수정</a>
 		<a href="delete?productNo=${productDto.productNo}">삭제</a>
+		<div>
+		<button class="btn-basket">담기</button>
+		</div>
 		<hr>
 		</div>
 	
