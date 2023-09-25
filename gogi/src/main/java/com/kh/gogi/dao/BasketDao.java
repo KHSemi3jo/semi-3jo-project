b8a8be8fa04e0d9ed446861b67b1a568534dbd05
MJH098 <narutoye@naver.com>
2023-09-25 15:03:45 +0900

    커밋

--- gogi/src/main/java/com/kh/gogi/dao/BasketDao.java
+++ gogi/src/main/java/com/kh/gogi/dao/BasketDao.java
@@ -6,6 +6,7 @@ import com.kh.gogi.dto.BasketDto;
 import com.kh.gogi.dto.BasketListDto;
 
 public interface BasketDao {
+	
 	int sequence();
 	void add(BasketDto basketDto);
 	boolean delete(int basketNo);
