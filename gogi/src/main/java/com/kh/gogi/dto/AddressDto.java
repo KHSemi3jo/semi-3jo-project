c77f8bb1524dd9360447e8ed793ea0313404a89b
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-09-26 20:29:56 +0900

    배송지 전송기능 빼고 거의다 구현

--- gogi/src/main/java/com/kh/gogi/dto/AddressDto.java
+++ gogi/src/main/java/com/kh/gogi/dto/AddressDto.java
@@ -10,6 +10,8 @@ import lombok.NoArgsConstructor;
 public class AddressDto {
 	private int addressNo;
 	private String addressId;
+	private String addressName;
+	private String addressPhone;
 	private String addressPost;
 	private String addressNormal;
 	private String addressDetail;
