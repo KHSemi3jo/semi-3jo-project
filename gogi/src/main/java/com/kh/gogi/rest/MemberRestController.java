1b33eb1a88af87d87821c77e9f3f04b5e0eaff62
smilesyk0323 <smilesyk0323@naver.com>
2023-09-30 01:24:03 +0900

    로그인 페이지 비밀번호 찾기 기능 구현
    - 기존 비밀번호 찾기 jsp파일 삭제
    - 비밀번호 찾기 팝업창으로 비동기 통신으로 기능 구현 완료

--- gogi/src/main/java/com/kh/gogi/rest/MemberRestController.java
+++ gogi/src/main/java/com/kh/gogi/rest/MemberRestController.java
@@ -1,7 +1,10 @@
 package com.kh.gogi.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +20,9 @@ public class MemberRestController {
 
 	@Autowired
 	private MemberDao memberDao;
+	
+	@Autowired
+	private JavaMailSender sender;
 	
 	@PostMapping("/idCheck")
 	public String idCheck(@RequestParam String memberId) {
@@ -37,6 +43,29 @@ public class MemberRestController {
 		}
 		else {
 			return "N";
+		}
+	}
+	
+	@PostMapping("/searchPw")
+	public String searchPw(@ModelAttribute MemberDto memberDto) {
+		//[1] 아이디로 모든 정보를 불러오고
+		MemberDto findDto = memberDao.selectOne(memberDto.getMemberId());
+		//[2] 이메일이 일치하는지 확인한다
+		boolean isValid = findDto != null  //아이디 존재+이메일 일치;
+					&& findDto.getMemberEmail().equals(memberDto.getMemberEmail());
+		if(isValid) { //이메일이 같다면
+			//이메일 발송 코드
+			SimpleMailMessage message = new SimpleMailMessage();
+			message.setTo(findDto.getMemberEmail());
+			message.setSubject("[고기어때]비밀번호찾기결과");
+			message.setText(findDto.getMemberPw());
+			sender.send(message);
+			
+			return "success";
+			
+		}
+		else { //이메일이 다르다면
+			return "아이디와 이메일주소를 확인해주세요";
 		}
 	}
 	
